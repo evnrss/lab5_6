1461eee0c599c71de123891a4dac074a05700c70
Leonid <leonidi368@gmail.com>
2023-04-12 10:52:48 +0300

    Добавили проверку заполненности полей формы и дату обращения пользователя в сокращённом формате

--- myform.py
+++ myform.py
@@ -1,12 +1,19 @@
 import re
+from datetime import datetime
 from bottle import post, request
 
-@post('/home')
+@post('/home', method='post')
+
 def my_form():
-    email = request.forms.get('ADRESS')
-    # Проверяем, соответствует ли email шаблону регулярного выражения
-    pattern = r"[^@]+@[^@]+\.[^@]+"
-    if re.match(pattern, email):
-        return "Thanks! The answer will be sent to the email %s" % email
-    else:
-        return "Invalid email address. Please enter a valid email address."
+    name = request.forms.get('NAME')
+    mail = request.forms.get('ADRESS')
+    # проверяем, заполнены ли оба поля формы
+    if not name or not mail:
+        return "Please fill in all fields"
+    else: # проверяем формат адреса электронной почты с помощью регулярного выражения
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", mail):
+            return "Invalid email format" # если формат некорректный, возвращаем сообщение об ошибке
+        else:
+            date = datetime.today().strftime('%Y-%m-%d') # получаем текущую дату
+            return "Thanks, %s! The answer will be sent to the mail %s. Access Date: %s" % (name, mail, date) 
+        # если все данные заполнены корректно, формируем и возвращаем сообщение с указанием имени, email и даты
--- views/index.tpl
+++ views/index.tpl
@@ -31,6 +31,7 @@
 <h3> Ask a Question </h3>
 <form action="/home" method="post">
         <p><textarea rows="2" cols="50" name="QUEST" placeholder="Your question" style="resize: none;"></textarea></p> 
+        <p><input type="text" size="50" name="NAME" placeholder="Your username"></p>
         <p><input type="text" size="50" name="ADRESS" placeholder="Your email"></p>
         <p><input type="submit" value="Send" class="btn btn-default"> </p>
 </form>
