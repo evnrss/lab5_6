ace9a140e041af763bcf9a44391ec5929c57ef5e
Leonid <leonidi368@gmail.com>
2023-04-12 10:41:37 +0300

    Добавили паттерн для адреса электронной почты (проверку на соответствие формату)

--- myform.py
+++ myform.py
@@ -1,6 +1,12 @@
+import re
 from bottle import post, request
 
 @post('/home')
 def my_form():
-    mail = request.forms.get('ADRESS')
-    return "Thanks! The answer will be sent to the email %s" % mail
+    email = request.forms.get('ADRESS')
+    # Проверяем, соответствует ли email шаблону регулярного выражения
+    pattern = r"[^@]+@[^@]+\.[^@]+"
+    if re.match(pattern, email):
+        return "Thanks! The answer will be sent to the email %s" % email
+    else:
+        return "Invalid email address. Please enter a valid email address."
